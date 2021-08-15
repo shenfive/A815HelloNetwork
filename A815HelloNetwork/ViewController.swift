40f0b376689cf19ba329b4e56207b0ace0b0a698
Danny Shen <shenfive@hotmail.com>
2021-08-15 11:33:11 +0800

    net work

--- A815HelloNetwork/ViewController.swift
+++ A815HelloNetwork/ViewController.swift
@@ -9,19 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var reachability: Reachability!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(getNetworkStatus())
-    }
 
-    
-    func getNetworkStatus()->Bool{
-        if Reachability(hostName: "www.apple.com")?.currentReachabilityStatus().rawValue == 0 {
-            return false
-        }else{
-            return true
-        }
+        reachability = Reachability(hostName: "http://www.apple.com")
+        reachability.startNotifier()
+        NotificationCenter.default.addObserver(
+            self,
+            selector:#selector(reachabilityChanged(note:)),
+            name: .reachabilityChanged,
+            object: reachability
+        )
     }
-
+    @objc func reachabilityChanged(note: NSNotification) {
+        print("Net work status Changed")
+        print(reachability.currentReachabilityStatus())
+        print()
+    }
 }
 
